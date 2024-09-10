a79aac7086f2d19910346bee8045b3b13aa9e285
jangelfdez <me@joseangelfernandez.es>
2024-09-10 14:18:18 +0200

    Added Chapter 7 content

---
+++ chapter7/AppInsightsExample/Pages/Privacy.cshtml.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppInsightsExample.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+    private readonly HttpClient _httpClient;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger, IHttpClientFactory httpClientFactory)
+    {
+        _logger = logger;
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    public async void OnGet()
+    {
+        string url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
+        var response = await _httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            _logger.LogInformation(content);
+        }
+    }
+    
+}
+
